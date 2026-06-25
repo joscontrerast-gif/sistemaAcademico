4ac40526b1c150808c3b6b07335f61da5b86ce61
josecontreras <jos.contrerast@duocuc.cl>
2026-06-25 13:04:50 -0400

    Docker Integrado y funcionando

--- ms-usuarios/src/test/java/com/saie/usuarios/MsUsuariosApplicationTests.java
+++ ms-usuarios/src/test/java/com/saie/usuarios/MsUsuariosApplicationTests.java
@@ -2,7 +2,10 @@ package com.saie.usuarios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
 class MsUsuariosApplicationTests {
 
